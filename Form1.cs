a26ab912862f9fe98274a363676a52f4b37235dc
lavarack <31459617+lavarack@users.noreply.github.com>
2018-04-07 13:05:43 -0500

    added files

---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pocket_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool comp = false;
+        public float h1 = 0;
+        public string type = "Not";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 7;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 8;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 4;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 5;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 6;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 1;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 2;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 3;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + ".";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Length > 0)
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+                label2.Text = "No Problems";
+            } else
+            {
+                label2.Text = "Nothing to Delete!";
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            h1 = float.Parse(textBox1.Text);
+            comp = true;
+            textBox1.Clear();
+            type = "add";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (comp == true)
+            {
+                if(textBox1.Text.Length > 0)
+                {
+                    float h2 = float.Parse(textBox1.Text);
+                    textBox1.Clear();
+                    if (type == "add")
+                    {
+                        textBox1.Text = Convert.ToString(h1 + h2);
+                    }
+                    if (type == "sub")
+                    {
+                        textBox1.Text = Convert.ToString(h1 - h2);
+                    }
+                    if (type == "div")
+                    {
+                        textBox1.Text = Convert.ToString(h1 / h2);
+                    }
+                    if (type == "mul")
+                    {
+                        textBox1.Text = Convert.ToString(h1 * h2);
+                    }
+                    if (type == "pow")
+                    {
+                        textBox1.Text = Convert.ToString(Math.Pow(h1, h2));
+                    }
+                    label2.Text = "No Problems";
+                    comp = false;
+                } else
+                {
+                    label2.Text = "Text Box is Empty!";
+                }
+                
+            }
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            h1 = float.Parse(textBox1.Text);
+            comp = true;
+            textBox1.Clear();
+            type = "sub";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            h1 = float.Parse(textBox1.Text);
+            comp = true;
+            textBox1.Clear();
+            type = "mul";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            h1 = float.Parse(textBox1.Text);
+            comp = true;
+            textBox1.Clear();
+            type = "div";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3.14159265";
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            type = "sqrt";
+            if (type == "sqrt")
+                h1 = float.Parse(textBox1.Text);
+            {
+                textBox1.Text = Convert.ToString(Math.Sqrt(h1));
+            }
+            comp = false;
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            h1 = float.Parse(textBox1.Text);
+            comp = true;
+            textBox1.Clear();
+            type = "pow";
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "-";
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+            Info m = new Info();
+            m.Show();
+        }
+    }
+}
---
+++ Info.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Pocket_Calculator
+{
+    partial class Info
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Info));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(356, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pocket Calculator 1.0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "By Owen Bell, 2018";
+            // 
+            // Info
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(554, 209);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Info";
+            this.Text = "Info";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
